7b535c9cb1847796ae0b4baadcf046b0aaf1ac29
zer0325 <70240867+zer0325@users.noreply.github.com>
2025-04-18 00:31:22 +0800

    Add files via upload

---
+++ xtrakt.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAXLENGTH 1000
+#define DELIMITER '\"'
+
+int getString(char []);
+
+int main(int argc, char *argv[])
+{
+    char url[MAXLENGTH];
+    /* Get the string between the delimeter as a possible URL
+     * if the first two character of the string are "./" print the string.
+     *	    Do not print if the string is "./"
+     */
+    while (getString(url) > 0) { 
+	if (url[0] == '.' && url[1] == '/' && url[2] != '\0')
+	    printf("%s\n", url);
+	else if (url[0] == '/' && url[1] != '\0')
+	    printf("%s\n", url);
+    }
+
+    return 0;
+}
+
+int getString(char s[])
+{
+    int c, i;
+
+    while((c = getchar()) != EOF) {
+	if (c == DELIMITER) {
+	    /* start saving the characters */
+	    for (i = 0; (c = getchar()) != DELIMITER && c != EOF; i++)
+		s[i] = c;
+	    s[i] = '\0';
+	    return i;
+	}
+    }
+    return 0;
+}
