834aef3936c6ca1d5dcede55178e2a5203d8199a
zer0325 <zer0325PH@gmail.com>
2025-04-19 10:01:59 +0800

    Bug Fix for some valid strings that are not detected.

--- xtrakt.c
+++ xtrakt.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#define MAXLENGTH 1000
+#define MAXLENGTH 12000
 #define DELIMITER '\"'
 
 int getString(char []);
+int isRelativeURL(char []);
+enum {FALSE, TRUE};
 
 int main(int argc, char *argv[])
 {
     char url[MAXLENGTH];
-    printf("debug:\n");
     /* Get the string between the delimeter as a possible URL
      * if the first two character of the string are "./" print the string.
      *	    Do not print if the string is "./"
      */
-    while (getString(url) > 0) { 
-	if (url[0] == '.' && url[1] == '/' && url[2] != '\0')
-	    printf("%s\n", url);
-	else if (url[0] == '/' && url[1] != '\0')
+    while (getString(url) != EOF) { 
+	if (isRelativeURL(url))
 	    printf("%s\n", url);
     }
-
     return 0;
 }
 
@@ -32,8 +30,20 @@ int getString(char s[])
 	    for (i = 0; (c = getchar()) != DELIMITER && c != EOF; i++)
 		s[i] = c;
 	    s[i] = '\0';
+	    if (c == EOF)
+		return EOF;
 	    return i;
 	}
     }
-    return 0;
+    return EOF;
+}
+
+int isRelativeURL(char s[])
+{
+    if (s[0] == '.' && s[1] == '/' && s[2] != '\0')
+	return TRUE;
+    else if (s[0] == '/' && s[1] != '\0')
+	return TRUE;
+    else
+	return FALSE;
 }
